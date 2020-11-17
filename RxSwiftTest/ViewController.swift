a9129ec983b2b62da0d7dc2af842372a1fdd80a1
AFSopra <alvaro.ferrandez.sopra@gmail.com>
2020-11-17 14:06:47 +0100

    Initial Commit

---
+++ RxSwiftTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftTest
+//
+//  Created by sopra on 17/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
